e8ba4080fb1bd6a505553abfe1d3fedabc92f38a
Berliana noviansyah <noviansyahberliana@gmail.com>
2022-06-12 21:14:36 +0700

    add

---
+++ lab11_php_ci/ci4/app/Controllers/page.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini Adalah Halaman About Yang Menjelaskan Tentang Isi Halaman Ini.'
+        ]);
+    }
+    public function contact()
+    {
+        echo "Ini Halaman Contact";
+    }
+    public function faqs()
+    {
+        echo "Ini Halaman Faqs";
+    }
+    public function tos()
+    {
+        echo "Ini Halaman Term of Services";
+    }
+}
